8eb1322f83678f7eba4830aa2d16ad35f3eb7d8a
unknown <you@example.com>
2026-05-26 11:58:37 +0700

    awal

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,13 @@
 <?php
 
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_daftar";
+$user="root";
+$pass ="";
+$db="db_daftar";;
 
 $conn = mysqli_connect($host,$user,$pass,$db);
 
-if(!$conn){
-    die("Koneksi gagal");
+if (!$conn) {
+    die("koneksi gagal");
 }
-
 ?>
